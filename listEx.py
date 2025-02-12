655783a0d2c7bbef22858e68a70ca2f5a2fdbcb7
aziz <aziz.cse.cu@tgmail.com>
2025-02-12 18:00:07 +0600

    Simple dictioary database code added

--- listEx.py
+++ listEx.py
@@ -28,5 +28,46 @@
 countries = {'china': 143, 'india': 136, 'usa': 32, 'pakistan': 21}
 
 for key, value in countries.items():
-    print(key + " ==> " + str(value))
+    print(key," ==> ",value)
 
+print(countries.keys())
+
+for country in countries:
+    print(country,"=>",countries[country])
+
+user_input = input("Which task you want to perform? ")
+if user_input == "add":
+    country = input("Enter country name: ")
+    if country in countries:
+        print("Country already exists")
+        pass
+    else:
+        population = int(input("Enter population:"))
+        countries[country] = population
+        print("Country added successfully")
+elif user_input == "delete":
+    country = input("Enter country name: ")
+    if country in countries:
+        del countries[country]
+        print("Country deleted successfully")
+    else:
+        print("Country does not exist")
+elif user_input == "update":
+    country = input("Enter country name: ")
+    if country in countries:
+        population = int(input("Enter population: "))
+        countries[country] = population
+        print("Country updated successfully")
+    else:
+        print("Country does not exist")
+elif user_input == "query":
+    country = input("Enter country name: ")
+    if country in countries:
+        print("population of",country,"is",countries[country])
+    else:
+        print("Country does not exist")
+elif user_input == "print":
+    for key, value in countries.items():
+        print(key,"==>",value)
+else:
+    print("Invalid input, plase select from add, delete, update, query, print")
