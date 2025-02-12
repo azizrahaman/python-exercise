8f695a84ae594391cafd81fd938938f146d5dc93
aziz <aziz.cse.cu@tgmail.com>
2025-02-12 20:59:07 +0600

    Update program for continous operation

--- listEx.py
+++ listEx.py
@@ -27,47 +27,100 @@
 
 countries = {'china': 143, 'india': 136, 'usa': 32, 'pakistan': 21}
 
-for key, value in countries.items():
-    print(key," ==> ",value)
+def make_choice():
+    print("Select from the following options:")
+    print("1. add")
+    print("2. delete")
+    print("3. update")
+    print("4. query")
+    print("5. print")
+    print("exit")
+    return input("Enter your choice: ")
 
-print(countries.keys())
+def operation():
+    choice = make_choice()
+    if choice == "1":
+        country = input("Enter country name: ")
+        if country in countries:
+            print("Country already exists")
+            # operation()
+        else:
+            population = int(input("Enter population:"))
+            countries[country] = population
+            print("Country added successfully")
+            # operation()
+    elif choice == "2":
+        country = input("Enter country name: ")
+        if country in countries:
+            del countries[country]
+            print("Country deleted successfully")
+            # operation()
+        else:
+            print("Country does not exist")
+            # operation()
+    elif choice == "3":
+        country = input("Enter country name: ")
+        if country in countries:
+            population = int(input("Enter population: "))
+            countries[country] = population
+            print("Country updated successfully")
+            # operation()
+        else:
+            print("Country does not exist")
+            # operation()
+    elif choice == "4":
+        country = input("Enter country name: ")
+        if country in countries:
+            print("population of",country,"is",countries[country])
+            # operation()
+        else:
+            print("Country does not exist")
+            # operation()
+    elif choice == "5":
+        for key, value in countries.items():
+            print(key,"==>",value)
+        # operation()
+    elif choice == "exit":
+        return
+    else:
+        print("Invalid input, plase select from add, delete, update, query, print")
+        operation()
+    operation()
 
-for country in countries:
-    print(country,"=>",countries[country])
+operation()
 
-user_input = input("Which task you want to perform? ")
-if user_input == "add":
-    country = input("Enter country name: ")
-    if country in countries:
-        print("Country already exists")
-        pass
-    else:
-        population = int(input("Enter population:"))
-        countries[country] = population
-        print("Country added successfully")
-elif user_input == "delete":
-    country = input("Enter country name: ")
-    if country in countries:
-        del countries[country]
-        print("Country deleted successfully")
-    else:
-        print("Country does not exist")
-elif user_input == "update":
-    country = input("Enter country name: ")
-    if country in countries:
-        population = int(input("Enter population: "))
-        countries[country] = population
-        print("Country updated successfully")
-    else:
-        print("Country does not exist")
-elif user_input == "query":
-    country = input("Enter country name: ")
-    if country in countries:
-        print("population of",country,"is",countries[country])
-    else:
-        print("Country does not exist")
-elif user_input == "print":
-    for key, value in countries.items():
-        print(key,"==>",value)
-else:
-    print("Invalid input, plase select from add, delete, update, query, print")
+# if user_input == "add":
+#     country = input("Enter country name: ")
+#     if country in countries:
+#         print("Country already exists")
+#         pass
+#     else:
+#         population = int(input("Enter population:"))
+#         countries[country] = population
+#         print("Country added successfully")
+# elif user_input == "delete":
+#     country = input("Enter country name: ")
+#     if country in countries:
+#         del countries[country]
+#         print("Country deleted successfully")
+#     else:
+#         print("Country does not exist")
+# elif user_input == "update":
+#     country = input("Enter country name: ")
+#     if country in countries:
+#         population = int(input("Enter population: "))
+#         countries[country] = population
+#         print("Country updated successfully")
+#     else:
+#         print("Country does not exist")
+# elif user_input == "query":
+#     country = input("Enter country name: ")
+#     if country in countries:
+#         print("population of",country,"is",countries[country])
+#     else:
+#         print("Country does not exist")
+# elif user_input == "print":
+#     for key, value in countries.items():
+#         print(key,"==>",value)
+# else:
+#     print("Invalid input, plase select from add, delete, update, query, print")
